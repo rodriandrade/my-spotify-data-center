aa6aea231f6eeed2303814e995e57ce37be1587a
rodriandrade <rodriandrade1498@gmail.com>
2021-03-15 02:18:22 -0300

    another try

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,29 +5,6 @@ import ba64 from 'ba64'
 import Cors from 'cors'
 import NextCors from 'nextjs-cors';
 
-/*
-// Initializing the cors middleware
-const cors = Cors({
-  methods: ['GET', 'HEAD', 'POST'],
-})
-*/
-
-/*
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-
-      return resolve(result)
-    })
-  })
-}
-*/
-
 const client = new Twitter({
   consumer_key: "LvUGWEPM8jgKcjTCWOOAoEdpx",
   consumer_secret: "6ess9trsAYNjDxAB2dmLsCpBuQaj4h2PUsVbVTTf9PR3pCuvBA",
@@ -35,24 +12,12 @@ const client = new Twitter({
   access_token_secret: "L5GAgiYeJsYKhWptB52ArO5kGrEfy8OhRhk6DkSGuA0lg"
 });
 
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: '100mb',
-    },
-  },
-}
-
 export default async (req, res) => {
-
-  await NextCors(req, res, {
-    // Options
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: '*',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  });
+  
  // await runMiddleware(req, res, cors)
   if (req.method === 'POST') {
+    res.send("Funciona");
+    /*
     const { dataUrl, tweet } = req.body;
   // console.log(dataUrl);
   console.log("hola")
@@ -107,5 +72,6 @@ export default async (req, res) => {
       //deleteImage();
     });
   });
+  */
   }
 }
